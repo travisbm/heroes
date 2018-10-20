de4d85e48744711b7c4385abf2d0eee5823d2172
Travis Montgomery <travisbm@gmail.com>
2018-10-20 18:05:16 -0500

    master/detail components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 3, name: "Birdman" },
+  { id: 2, name: "Homer" },
+  { id: 5, name: "Biscuit" },
+  { id: 9, name: "Trainwreck" },
+  { id: 39, name: "Treestump" },
+  { id: 33, name: "Suckerpunch" },
+  { id: 29, name: "Skittles" },
+  { id: 26, name: "Cartrunk" },
+  { id: 44, name: "Wheelbarrow" },
+  { id: 19, name: "Racecar" }
+];
